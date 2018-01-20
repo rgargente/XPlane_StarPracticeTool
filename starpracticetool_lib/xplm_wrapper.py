d29c675796a7f0308b4dffb27d894bec4e7bfea6
rgargente <rgargente@gmail.com>
2018-01-20 17:43:27 +0000

    Fixes for xplm_wrapper as I start to try it

--- starpracticetool_lib/xplm_wrapper.py
+++ starpracticetool_lib/xplm_wrapper.py
@@ -3,6 +3,7 @@ This is the code that deals directly with the X-Plane SDK. It is not easily test
 It is created as a class instead of a simple collection of functions so that we can use a mock object for testing purposes.
 """
 from XPLMNavigation import *
+from XPLMDataAccess import *
 
 
 # xplm_Nav_Unknown: "UKN",
@@ -23,7 +24,8 @@ class XplmWrapper:
 
     def get_waypoint_lat_lon(self, id, airport_lat, airport_lon):
         ref = XPLMFindNavAid(None, id, airport_lat, airport_lon,
-                             None, [xplm_Nav_Airport, xplm_Nav_NDB, xplm_Nav_VOR, xplm_Nav_Fix])
+                             None, xplm_Nav_Airport | xplm_Nav_NDB | xplm_Nav_VOR | xplm_Nav_Fix)
+        # None, xplm_Nav_Airport)
         out_lat, out_long = [], []
         XPLMGetNavAidInfo(ref, None, out_lat, out_long, None, None, None, None, None, None)
         return out_lat[0], out_long[0]
@@ -34,11 +36,11 @@ class XplmWrapper:
         return XPLMGetDataf(lat_dr), XPLMGetDataf(lon_dr)
 
     def get_nearest_airport(self):
-        lat, lon = get_aircraft_lat_lon()
+        lat, lon = self.get_aircraft_lat_lon()
         ref = XPLMFindNavAid(None, None, lat, lon, None, xplm_Nav_Airport)
         if ref == XPLM_NAV_NOT_FOUND:
             return None, None
         else:
-            out_lat, out_lon, out_id, out_name = [], [], [], [], []
+            out_lat, out_lon, out_id, out_name = [], [], [], []
             XPLMGetNavAidInfo(ref, None, out_lat, out_lon, None, None, None, out_id, out_name, None)
             return out_id[0], out_name[0], out_lat[0], out_lon[0]
