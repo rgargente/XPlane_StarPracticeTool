f9b65782cd55a45019fde685911109a1b183f32d
rgargente <rgargente@gmail.com>
2018-07-22 18:30:17 +0100

    fixparser

--- starpracticetool_lib/fixparser.py
+++ starpracticetool_lib/fixparser.py
@@ -1,12 +1,20 @@
+import re
+
 from starpracticetool_lib.mathlib import is_number
 
 
 class FixParser:
-    def __init__(self, filepath):
-        self.fixes = []
-        with open(filepath) as f:
+    def __init__(self, fix_filepath):
+        self.fixes = {}
+
+        with open(fix_filepath) as f:
             for l in f:
+                l = re.sub(' +', ' ', l.strip())  # Remove multiple spaces
                 parts = l.split(' ', 5)
                 if len(parts) == 5 \
                         and is_number(parts[0]) and is_number(parts[1]):
-                    self.fixes.append((float(parts[0]), float(parts[1]), parts[2]))
+                    self.fixes[parts[2]] = (float(parts[0]), float(parts[1]))
+
+
+    def get_coord(self, fixname):
+        return self.fixes[fixname]
