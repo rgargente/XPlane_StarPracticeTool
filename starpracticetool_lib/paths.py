aaea5e3340abf6c1acf8b3314def065f7ec1bd54
rgargente <rgargente@gmail.com>
2018-08-23 21:06:07 +0100

    get paths for nav dat files

--- starpracticetool_lib/paths.py
+++ starpracticetool_lib/paths.py
@@ -1,9 +1,29 @@
 import os
 
 
-def get_airport_file_path(xplane_path, airport_icao):
-    file_name = "{}.dat".format(airport_icao)
-    file_path = os.path.join(xplane_path, "Custom Data", "CIFP", file_name)
+def get_default_data_path(xplane_path):
+    return os.path.join(xplane_path, "Resources", "default data")
+
+
+def get_custom_data_path(xplane_path):
+    return os.path.join(xplane_path, "Custom Data")
+
+
+def _get_file_path(xplane_path, file_name):
+    file_path = os.path.join(get_custom_data_path(xplane_path), file_name)
     if not os.path.exists(file_path):
-        file_path = os.path.join(xplane_path, "Resources", "default data", "CIFP", file_name)
+        file_path = os.path.join(get_default_data_path(xplane_path), file_name)
     return file_path
+
+
+def get_airport_file_path(xplane_path, airport_icao):
+    file_name = os.path.join("CIFP", "{}.dat".format(airport_icao))
+    return _get_file_path(xplane_path, file_name)
+
+
+def get_earth_fix_dat_file_path(xplane_path):
+    return _get_file_path(xplane_path, "earth_fix.dat")
+
+
+def get_earth_nav_dat_file_path(xplane_path):
+    return _get_file_path(xplane_path, "earth_nav.dat")
--- starpracticetool_lib/test/test_paths.py
+++ starpracticetool_lib/test/test_paths.py
@@ -5,42 +5,57 @@ import pytest
 from starpracticetool_lib import paths
 
 
-def mock_file_exists(monkeypatch, custom_file_path, default_file_path, custom_exists):
+def mock_file_exists(monkeypatch, default_file_path, custom_file_path, custom_exists):
     def mock_exists(path):
-        if path == custom_file_path:
-            return custom_exists
         if path == default_file_path:
             return True
+        elif path == custom_file_path:
+            return custom_exists
         return False
 
     monkeypatch.setattr(os.path, 'exists', mock_exists)
 
 
-@pytest.fixture()
-def xplane_path():
-    return 'xplanepath/'
+XPLANE_PATH = 'xplanepath/'
+AIRPORT_ICAO = 'LEBB'
+
+CUSTOM_PATH = os.path.join(XPLANE_PATH, "Custom Data")
+DEFAULT_PATH = os.path.join(XPLANE_PATH, "Resources", "default data")
+
+CUSTOM_APT_PATH = os.path.join(CUSTOM_PATH, "CIFP", "LEBB.dat")
+DEFAULT_APT_PATH = os.path.join(DEFAULT_PATH, "CIFP", "LEBB.dat")
+
+DEFAULT_EARTH_FIX = os.path.join(DEFAULT_PATH, "earth_fix.dat")
+CUSTOM_EARTH_FIX = os.path.join(CUSTOM_PATH, "earth_fix.dat")
+DEFAULT_EARTH_NAV = os.path.join(DEFAULT_PATH, "earth_nav.dat")
+CUSTOM_EARTH_NAV = os.path.join(CUSTOM_PATH, "earth_nav.dat")
 
 
-@pytest.fixture()
-def airport_icao():
-    return 'LEBB'
+def test_custom_airport_file_is_prioritised(monkeypatch):
+    mock_file_exists(monkeypatch, DEFAULT_APT_PATH, CUSTOM_APT_PATH, custom_exists=True)
+    assert paths.get_airport_file_path(XPLANE_PATH, AIRPORT_ICAO) == CUSTOM_APT_PATH
 
 
-@pytest.fixture()
-def custom_file_path(xplane_path):
-    return os.path.join(xplane_path, "Custom Data", "CIFP", "LEBB.dat")
+def test_default_airport_file_is_used(monkeypatch):
+    mock_file_exists(monkeypatch, DEFAULT_APT_PATH, CUSTOM_APT_PATH, custom_exists=False)
+    assert paths.get_airport_file_path(XPLANE_PATH, AIRPORT_ICAO) == DEFAULT_APT_PATH
 
 
-@pytest.fixture()
-def default_file_path(xplane_path):
-    return os.path.join(xplane_path, "Resources", "default data", "CIFP", "LEBB.dat")
+def test_custom_earth_fix_dat_file_is_prioritised(monkeypatch):
+    mock_file_exists(monkeypatch, DEFAULT_EARTH_FIX, CUSTOM_EARTH_FIX, custom_exists=True)
+    assert paths.get_earth_fix_dat_file_path(XPLANE_PATH) == CUSTOM_EARTH_FIX
 
 
-def test_custom_data_folder_is_prioritised(monkeypatch, xplane_path, custom_file_path, default_file_path, airport_icao):
-    mock_file_exists(monkeypatch, custom_file_path, default_file_path, True)
-    assert paths.get_airport_file_path(xplane_path, airport_icao) == custom_file_path
+def test_default_earth_fix_dat_file_is_used(monkeypatch):
+    mock_file_exists(monkeypatch, DEFAULT_EARTH_FIX, CUSTOM_EARTH_FIX, custom_exists=False)
+    assert paths.get_earth_fix_dat_file_path(XPLANE_PATH) == DEFAULT_EARTH_FIX
 
 
-def test_default_data_is_used(monkeypatch, xplane_path, custom_file_path, default_file_path, airport_icao):
-    mock_file_exists(monkeypatch, custom_file_path, default_file_path, False)
-    assert paths.get_airport_file_path(xplane_path, airport_icao) == default_file_path
+def test_custom_earth_nav_dat_file_is_prioritised(monkeypatch):
+    mock_file_exists(monkeypatch, DEFAULT_EARTH_NAV, CUSTOM_EARTH_NAV, custom_exists=True)
+    assert paths.get_earth_nav_dat_file_path(XPLANE_PATH) == CUSTOM_EARTH_NAV
+
+
+def test_default_earth_nav_dat_file_is_used(monkeypatch):
+    mock_file_exists(monkeypatch, DEFAULT_EARTH_NAV, CUSTOM_EARTH_NAV, custom_exists=False)
+    assert paths.get_earth_nav_dat_file_path(XPLANE_PATH) == DEFAULT_EARTH_NAV
